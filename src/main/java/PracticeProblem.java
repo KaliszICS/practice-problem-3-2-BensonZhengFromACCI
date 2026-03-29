48612750e5044327f71aa7dfee97ae64594ab3e6
BensonZhengFromACCI <343804126@tdsb.ca>
2026-03-29 05:20:08 +0000

    hi mr kalisz i might sleep soon

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,6 +4,44 @@ public class PracticeProblem {
 
 	}
 
+public static String isFive(int numIn) {
+	if (numIn == 5) {
+		return "The number is Five";
+	}
+	else {
+		return "The number is not Five";
+	}
+
+}
 	
+public static String positiveOrNegative(double numIn) {
+	if (numIn > 0) {
+		return "Positive";
+	}
+	else {
+		return "Negative";
+	}
+
+}
+
+public static String highOrLow(int numIn) {
+	if (numIn > 100) {
+		return "High";
+	}
+	else {
+		return "Low";
+	}
+
+}
+
+public static String isHello(String wordIn) {
+	if (wordIn == "Hello") {
+		return "The word is Hello";
+	}
+	else {
+		return "The word is not Hello";
+	}
+
+}
 
 }
